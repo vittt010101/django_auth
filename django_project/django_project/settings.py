1b0aeb772ed1ee59cf898c42f89f93bc2431471f
Tuong Vi <127405773+vittt010101@users.noreply.github.com>
2023-03-11 03:03:13 +0900

    Adjust push heroku master

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -20,8 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-14vti6x+r6isbdi9q4)lm4tdsx3*9ag0vdl(6u1*!8sf_%t2c-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
